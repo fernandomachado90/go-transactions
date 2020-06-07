63e99fa28bde0af12cc2a42df1d6f761ed9e725f
Fernando Machado <lfmachado90@gmail.com>
2020-06-07 15:44:50 -0300

    Adds Create Account functionality.

--- core/account.go
+++ core/account.go
@@ -4,3 +4,18 @@ type Account struct {
 	ID             int
 	DocumentNumber string
 }
+
+type AccountManager struct {
+	db interface {
+		CreateAccount(Account) (Account, error)
+	}
+}
+
+func (m *AccountManager) Create(account Account) (Account, error) {
+	account, err := m.db.CreateAccount(account)
+	if err != nil {
+		return Account{}, err
+	}
+
+	return account, nil
+}
--- core/account_test.go
+++ core/account_test.go
@@ -1,21 +1,23 @@
 package core
 
 import (
+	"errors"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestNewAccount(t *testing.T) {
-
 	tests := map[string]func(*testing.T){
 		"Should initialize entity": func(t *testing.T) {
 			//given
 			id := 1
-			documentNumber := "12345678900"
+			documentNumber := "1234567890"
 
 			// when
-			account := &Account{
+			account := Account{
 				ID:             id,
 				DocumentNumber: documentNumber,
 			}
@@ -30,4 +32,65 @@ func TestNewAccount(t *testing.T) {
 			run(t)
 		})
 	}
+}
+
+func TestCreateAccount(t *testing.T) {
+
+	tests := map[string]func(*testing.T){
+		"Should create account successfully": func(t *testing.T) {
+			//given
+			input := Account{
+				DocumentNumber: "1234567890",
+			}
+			db := new(dbMock)
+			db.On("CreateAccount", input).Return(rand.Int(), nil)
+			accountManager := AccountManager{db: db}
+
+			// when
+			output, err := accountManager.Create(input)
+
+			// then
+			assert.NotEmpty(t, output.ID)
+			assert.NoError(t, err)
+		},
+		"Should not create account and return error": func(t *testing.T) {
+			//given
+			input := Account{
+				DocumentNumber: "1234567890",
+			}
+			db := new(dbMock)
+			db.On("CreateAccount", input).Return(0, errors.New("error"))
+			accountManager := AccountManager{db: db}
+
+			// when
+			output, err := accountManager.Create(input)
+
+			// then
+			assert.Empty(t, output.ID)
+			assert.Error(t, err)
+		},
+	}
+
+	for name, run := range tests {
+		t.Run(name, func(t *testing.T) {
+			run(t)
+		})
+	}
+}
+
+type dbMock struct {
+	mock.Mock
+}
+
+func (m *dbMock) CreateAccount(account Account) (Account, error) {
+	args := m.Called(account)
+	id := args.Int(0)
+	err := args.Error(1)
+
+	if err != nil {
+		return Account{}, err
+	}
+
+	account.ID = id
+	return account, nil
 }
